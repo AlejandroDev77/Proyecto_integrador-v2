ca2a535ee33e0b68f0573b0b298b0dddd8714c94
Your Namepakazzz69 <you@example.com>
2026-03-09 20:32:57 -0400

    ruta

--- backend/src/main/java/com/changuitostudio/backend/infrastructure/adapter/in/web/controller/RolesController.java
+++ backend/src/main/java/com/changuitostudio/backend/infrastructure/adapter/in/web/controller/RolesController.java
@@ -6,7 +6,7 @@ import com.changuitostudio.backend.infrastructure.adapter.out.persistence.reposi
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+//import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
